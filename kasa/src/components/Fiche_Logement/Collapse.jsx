38b17cff509d57eb83b6088ae422b2b732c017db
Alexandre Detoc <alexandre.detoc@hotmail.fr>
2023-05-05 12:27:25 +0200

    fonction collapse

--- kasa/src/components/Fiche_Logement/Collapse.jsx
+++ kasa/src/components/Fiche_Logement/Collapse.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import { useState } from 'react';
-import { children } from 'react';
+import { useState, useRef } from 'react';
+import './collapse.css'
 
 const Collapsible = (props) => {
 
     const toggle = () => {
         setOPen(!open);
       };
+    
+    const contentRef = useRef();
+      if (contentRef.current) console.log(contentRef.current.scrollHeight);
 
     const [open, setOPen] = useState(false);
         return (
             <div>
                 <button onClick={toggle}>{props.label}</button>
             {open && 
-                <div className="toggle">{props.children}</div>             
+            <div  className={open ? "content-show" : "content-parent"}>
+              
+                    <div className="content">{props.children}</div>   
+                
+            </div>          
             }
             </div>
         )
