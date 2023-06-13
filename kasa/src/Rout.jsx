24c1ae7224d51a8c496d629f803bda30cd4ebd91
Alexandre Detoc <alexandre.detoc@hotmail.fr>
2023-06-13 11:40:52 +0200

    separation fichier rout et fix affichage collapse

---
+++ kasa/src/Rout.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home.jsx';
+import Error from './pages/Error';
+import FicheLogement from './pages/FicheLogement' 
+import Apropos from './pages/Apropos'
+
+import Header from './components/Header/index'
+import Footer from './components/Footer';
+
+import "./style.css"
+
+
+export default function Rout() {
+    return ( 
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/FicheLogement/:id' element={<FicheLogement />} />
+          <Route path='Apropos' element={<Apropos />} />
+          <Route path='*' element={<Error />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    )
+  }
